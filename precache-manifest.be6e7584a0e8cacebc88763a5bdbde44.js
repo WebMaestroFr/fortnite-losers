a8eba66e108a5d5a40835e8886d349e2ca3ca1af
Etienne <etienne@visulytix.com>
2020-02-16 11:49:06 +0100

    Updates

---
+++ precache-manifest.be6e7584a0e8cacebc88763a5bdbde44.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b55722c44f731269f1a98a91a4a8290",
+    "url": "/fortnite-losers/index.html"
+  },
+  {
+    "revision": "418ea2b2075ffb31e62a",
+    "url": "/fortnite-losers/static/css/main.86d4595f.chunk.css"
+  },
+  {
+    "revision": "dfadffb12e5510a737c2",
+    "url": "/fortnite-losers/static/js/2.fa1c0537.chunk.js"
+  },
+  {
+    "revision": "efc015cd9ba288fae3493ffe257b695b",
+    "url": "/fortnite-losers/static/js/2.fa1c0537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "418ea2b2075ffb31e62a",
+    "url": "/fortnite-losers/static/js/main.55a1d37d.chunk.js"
+  },
+  {
+    "revision": "cb93d369013643db2989",
+    "url": "/fortnite-losers/static/js/runtime-main.d5753d10.js"
+  },
+  {
+    "revision": "089f97c8679c427453d7dc2960e04634",
+    "url": "/fortnite-losers/static/media/BurbankBigRegular-Black.089f97c8.woff"
+  },
+  {
+    "revision": "5cb5290e3a11802dfe43d8d07c344196",
+    "url": "/fortnite-losers/static/media/BurbankBigRegular-Black.5cb5290e.ttf"
+  },
+  {
+    "revision": "67b25e1d029504097d50c9964fad1655",
+    "url": "/fortnite-losers/static/media/BurbankBigRegular-Black.67b25e1d.eot"
+  },
+  {
+    "revision": "8dd35a5ee9eab8c7c069365ac14fed9a",
+    "url": "/fortnite-losers/static/media/BurbankBigRegular-Black.8dd35a5e.otf"
+  },
+  {
+    "revision": "b60d2274b782c64bbe0f39c065f1bfa5",
+    "url": "/fortnite-losers/static/media/BurbankBigRegular-Black.b60d2274.svg"
+  }
+]);
